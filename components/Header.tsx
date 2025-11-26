e0792cd11927d03d7f1972eb5926460b7113bc60
m3nd0zit4 <julianestebanmb2007@gmail.com>
2025-11-25 19:17:05 -0500

    feat: implement sign-up functionality and user session management

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import NavItems from "./NavItems";
 import UserDropdown from "./UserDropdown";
 
-const Header = () => {
+const Header = ({ user }: { user: User }) => {
 
     return (
         <header className="sticky top-0 header">
@@ -15,7 +15,7 @@ const Header = () => {
                     <NavItems />
                 </nav>
 
-                <UserDropdown />
+                <UserDropdown user={user} />
             </div>
         </header>
     )
